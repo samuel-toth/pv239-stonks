8362f967ebb1db714769dc414e1806f7b5af5a1b
Samuel Tóth <mr.samueltoth@gmail.com>
2023-03-10 12:33:51 +0100

    Initial Commit

---
+++ Stonks/StonksApp.swift
@@ -0,0 +1,20 @@
+//
+//  StonksApp.swift
+//  Stonks
+//
+//  Created by Samuel Tóth on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct StonksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
